ad69a120b5d1e828f28d6e3db6559daccbfe3500
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-11-12 18:48:55 -0300

    Inicio do projeto de reconhecimento de imagens de letras e numeros usando vision

---
+++ ImageTextRecognition/ImageTextRecognition/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ImageTextRecognition
+//
+//  Created by Ronaldo Gomes on 12/11/20.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController {
+    
+    var model: VNCoreMLModel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
