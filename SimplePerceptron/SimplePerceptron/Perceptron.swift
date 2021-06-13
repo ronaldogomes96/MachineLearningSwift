5e7413c2eeb02ebe9646814efc19f8ebb25dc328
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2021-06-13 00:46:43 -0300

    Project with a simple perceptron implementation

---
+++ SimplePerceptron/SimplePerceptron/Perceptron.swift
@@ -0,0 +1,82 @@
+//
+//  Perceptron.swift
+//  SimplePerceptron
+//
+//  Created by Ronaldo Gomes on 12/06/21.
+//
+
+import Foundation
+
+class Perceptron {
+    private var weights = [Double]()
+    private var rate: Double
+    private var epochs: Int
+    
+    init(rate: Double = 0.5, epochs: Int = 10000) {
+        self.rate = rate
+        self.epochs = epochs
+    }
+    
+    func predict(data: [Double]) -> Int {
+        var sumOfWeights: Double = 0.0
+        
+        for (position, x) in data.enumerated() {
+            sumOfWeights += x * weights[position]
+        }
+        
+        return sigmoid(sum: sumOfWeights)
+    }
+    
+    func train(data: [[Double]], targets: [Int]) {
+        var sumOfWeights: Double = 0.0
+        var hadErros = [Bool]()
+        
+        data[0].forEach { _ in
+            weights.append(Double.random(in: -1...1))
+        }
+        
+        for epoch in 0...epochs {
+            hadErros = []
+            
+            for (positionLine, line) in data.enumerated() {
+                for (posi, x) in line.enumerated() {
+                    sumOfWeights += x * weights[posi]
+                }
+                
+                //let sinal = degree(sum: sumOfWeights)
+                let sinal = sigmoid(sum: sumOfWeights)
+                print("\(sinal) \(targets[positionLine])")
+                
+                if sinal != targets[positionLine] {
+                    updateWeights(xData: data[positionLine], expectSinal: targets[positionLine], realSinal: sinal)
+                    hadErros.append(true)
+                } else {
+                    hadErros.append(false)
+                }
+            }
+            
+            print("Epoch \(epoch) \nWeights \(weights)\n\n")
+            
+            if !hadErros.contains(true) {
+                return
+            }
+        }
+        
+    }
+    
+    private func degree(sum: Double) -> Int {
+        return sum >= 0 ? 1 : 0
+    }
+    
+    private func sigmoid(sum: Double) -> Int {
+        let sigmoid = 1 / (1 + exp(sum))
+        return degree(sum: sigmoid)
+    }
+    
+    private func updateWeights(xData: [Double], expectSinal: Int, realSinal: Int) {
+        let error = expectSinal - realSinal
+        for (position, x) in xData.enumerated() {
+            weights[position] = weights[position] + (rate * Double(error) * x)
+        }
+    }
+}
---
+++ SimplePerceptron/SimplePerceptron/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  SimplePerceptron
+//
+//  Created by Ronaldo Gomes on 12/06/21.
+//
+
+import Foundation
+
+let data = [ [0.3, 0.7],
+             [-0.6, 0.3],
+             [-0.1, -0.8],
+             [0.1, -0.45] ]
+
+let targets = [1, 0, 0 ,1]
+
+let perceptron = Perceptron()
+
+perceptron.train(data: data, targets: targets)
